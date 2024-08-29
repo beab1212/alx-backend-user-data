56ce21b88694f43d004273db15aabd18c06301fa
Mr-robot <hackingtest017@gmail.com>
2024-08-29 17:43:26 +0300

    add encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,5 +6,6 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """ Encrypt and salt user password """
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
