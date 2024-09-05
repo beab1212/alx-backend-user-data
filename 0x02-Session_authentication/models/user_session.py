250f00561c88fd234f14f71de517346c3dbcb791
Mr-robot <hackingtest017@gmail.com>
2024-09-05 21:06:29 +0300

    initialiation fix

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -8,6 +8,8 @@ class UserSession(Base):
     """User session data model
     """
     def __init__(self, *args: list, **kwargs: dict):
+        """ser session Initialilizer
+        """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
