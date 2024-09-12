09a8570f5cfc72edcb4dd9e7922252318df38e72
Mr-robot <hackingtest017@gmail.com>
2024-09-12 09:12:16 +0300

    deleting session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """deleting session_id by user id
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            pass
+        return None
