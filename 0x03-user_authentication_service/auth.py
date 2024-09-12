8c86588929684811fc5d52a9eacfe024f2fb5052
Mr-robot <hackingtest017@gmail.com>
2024-09-12 09:38:44 +0300

    implement reset password token generator

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,3 +84,14 @@ class Auth:
         except ValueError:
             pass
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """generate reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = str(uuid4())
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
