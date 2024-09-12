feea1ae4532c4b522a645c1bb867527cf57f6096
Mr-robot <hackingtest017@gmail.com>
2024-09-12 08:56:57 +0300

    authorization routes

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Flask web server
+"""
+from flask import Flask, jsonify, request, abort
+from auth import Auth
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """home route handler
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """users post handler
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError as error:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """login handler
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if email is None or password is None:
+        abort(401)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_token = AUTH.create_session(email)
+    res = jsonify({"email": f"{email}", "message": "logged in"})
+    res.set_cookie('session_id', session_token)
+    return res
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """Authentication provider
 """
 import bcrypt
+from uuid import uuid4
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -13,6 +14,13 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt(rounds=5)
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def _generate_uuid() -> str:
+    """generate uuid
+    """
+    uuid_str = str(uuid4())
+    return uuid_str
 
 
 class Auth:
@@ -32,3 +40,27 @@ class Auth:
         except NoResultFound:
             new_user = self._db.add_user(email, _hash_password(password))
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate credentials
+        """
+        try:
+            is_user_exist = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'),
+                              is_user_exist.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """ create and attach session token to a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_token = _generate_uuid()
+            self._db.update_user(user_id=user.id, session_id=session_token)
+            return session_token
+        except NoResultFound:
+            return None
